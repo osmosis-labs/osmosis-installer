1ec7e59359433f4683aa199149190c69b943eda5
Adam Tucker <adamleetucker@outlook.com>
2022-05-19 09:20:57 -0500

    revernt tesnet to v7

--- osmosis-installer.py
+++ osmosis-installer.py
@@ -10,7 +10,7 @@ from sys import argv
 from os import remove
 
 # self-destruct file after first call
-remove(argv[0])
+#remove(argv[0])
 
 class CustomHelpFormatter(argparse.HelpFormatter):
     def _format_action_invocation(self, action):
@@ -1117,8 +1117,8 @@ def initSetup ():
             print(bcolors.OKGREEN + "(5/5) Installing Osmosis v8.0.0 Binary..." + bcolors.ENDC)
             subprocess.run(["git checkout v8.0.0"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         if networkAns == "2":
-            print(bcolors.OKGREEN + "(5/5) Installing Osmosis v8.0.0 Binary..." + bcolors.ENDC)
-            subprocess.run(["git checkout v8.0.0"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+            print(bcolors.OKGREEN + "(5/5) Installing Osmosis v7.3.0 Binary..." + bcolors.ENDC)
+            subprocess.run(["git checkout v7.3.0"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         if networkAns == "3" and versionAns == "1":
             print(bcolors.OKGREEN + "(5/5) Installing Osmosis Binary..." + bcolors.ENDC)
             subprocess.run(["git checkout "+osmoVersion], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
@@ -1163,8 +1163,8 @@ def initSetup ():
             print(bcolors.OKGREEN + "(4/4) Installing Osmosis v8.0.0 Binary..." + bcolors.ENDC)
             subprocess.run(["git checkout v8.0.0"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         elif networkAns == "2" and machine != 'arm64':
-            print(bcolors.OKGREEN + "(4/4) Installing Osmosis v8.0.0 Binary..." + bcolors.ENDC)
-            subprocess.run(["git checkout v8.0.0"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+            print(bcolors.OKGREEN + "(4/4) Installing Osmosis v7.3.0 Binary..." + bcolors.ENDC)
+            subprocess.run(["git checkout v7.3.0"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         elif networkAns == "3" and versionAns == "1":
             print(bcolors.OKGREEN + "(4/4) Installing Osmosis Binary..." + bcolors.ENDC)
             subprocess.run(["git checkout "+osmoVersion], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
