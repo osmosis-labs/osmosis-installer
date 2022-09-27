82c98dff3f5560d99dfbf9a1f567ea287cfec218
Adam Tucker <adam@osmosis.team>
2022-09-27 13:00:19 -0500

    add comments

--- osmosis-installer.sh
+++ osmosis-installer.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# run get.osmosis.zone python script
 curl -sL https://get.osmosis.zone/install > i.py && python3 i.py
+
+# after completion, source the profile
 source ~/.profile
+
+# remove bash script
+rm -- "$0"
