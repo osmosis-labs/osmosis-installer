30434fc07592d3bbdbb2319abac7ee692de708f1
Niccolo Raspa <nraspa@live.it>
2023-07-13 10:45:05 +0200

    Update installer

--- installer.py
+++ installer.py
@@ -160,12 +160,12 @@ MAINNET = Network(
     genesis_url = "https://osmosis.fra1.digitaloceanspaces.com/osmosis-1/genesis.json",
     binary_url = {
         "linux": {
-            "amd64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.0.0/osmosisd-16.0.0-linux-amd64",
-            "arm64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.0.0/osmosisd-16.0.0-linux-arm64"
+            "amd64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.1.0/osmosisd-16.1.0-linux-amd64",
+            "arm64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.1.0/osmosisd-16.1.0-linux-arm64"
         },
         "darwin": {
-            "amd64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.0.0/osmosisd-16.0.0-darwin-amd64",
-            "arm64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.0.0/osmosisd-16.0.0-darwin-arm64"
+            "amd64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.1.0/osmosisd-16.1.0-darwin-amd64",
+            "arm64": "https://github.com/osmosis-labs/osmosis/releases/download/v16.1.0/osmosisd-16.1.0-darwin-arm64"
         },
     },
     peers = None,
