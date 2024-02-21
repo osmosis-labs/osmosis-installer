a64d4b60b0f437ce1e702bdca6bbf2cecced7a77
Niccolo Raspa <niccoloraspa@gmail.com>
2024-02-21 09:41:13 +0100

    Update installer to 23.0.0

--- i.py
+++ i.py
@@ -18,8 +18,8 @@ NETWORK_CHOICES = ['osmosis-1', 'osmo-test-5']
 INSTALL_CHOICES = ['node', 'client', 'localosmosis']
 PRUNING_CHOICES = ['default', 'nothing', 'everything']
 
-MAINNET_VERSION = "22.0.3"
-TESTNET_VERSION = "22.0.0-rc0"
+MAINNET_VERSION = "23.0.0"
+TESTNET_VERSION = "23.0.0-rc0"
 
 # CLI arguments
 parser = argparse.ArgumentParser(description="Osmosis Installer")
