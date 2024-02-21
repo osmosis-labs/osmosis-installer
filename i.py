b6c7a0c7a474bd2061bea5d069c6c4c5ee25b748
Niccolo Raspa <niccoloraspa@gmail.com>
2024-02-21 09:44:36 +0100

    Update testnet installer to 23.0.0-rc1

--- i.py
+++ i.py
@@ -19,7 +19,7 @@ INSTALL_CHOICES = ['node', 'client', 'localosmosis']
 PRUNING_CHOICES = ['default', 'nothing', 'everything']
 
 MAINNET_VERSION = "23.0.0"
-TESTNET_VERSION = "23.0.0-rc0"
+TESTNET_VERSION = "23.0.0-rc1"
 
 # CLI arguments
 parser = argparse.ArgumentParser(description="Osmosis Installer")
