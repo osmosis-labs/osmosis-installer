48c9d113e83258bbbd233a73737e03b45f97fb0a
niccoloraspa <6024049+niccoloraspa@users.noreply.github.com>
2024-06-23 00:02:56 +0000

    Update MAINNET_VERSION version to 25.1.1

--- i.py
+++ i.py
@@ -18,7 +18,7 @@ NETWORK_CHOICES = ['osmosis-1', 'osmo-test-5']
 INSTALL_CHOICES = ['node', 'client', 'localosmosis']
 PRUNING_CHOICES = ['default', 'nothing', 'everything']
 
-MAINNET_VERSION = "25.1.2"
+MAINNET_VERSION = "25.1.1"
 TESTNET_VERSION = "25.1.2"
 
 # CLI arguments
